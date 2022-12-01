3bf86b961d041af0e42172fd380bb63370932522
Merdi-kim <merdikimuha1@gmail.com>
2022-12-01 12:38:44 +0300

    set contract underlying behavior

--- contracts/AfricaDao.sol
+++ contracts/AfricaDao.sol
@@ -8,11 +8,13 @@ contract AfricaDao {
 
     struct Proposal {
         string url;
+        address payable proposalOwner;
+        uint256 votesFor;
+        uint256 votesAgainst;
+        uint256 fundsRaised;
     }
 
-    mapping(uint256 => Proposal) proposals;
-
-
+    mapping(address => Proposal) proposals;
 
     event ProposalCreated();
     event VotedFor();
@@ -22,27 +24,39 @@ contract AfricaDao {
     event FundsDistributed();
 
     function createProposal(string memory _proposalUrl) external {
-
+        //require(condition);   @condition to make sure the same proposal link is not submitted twice 
+        proposals[msg.sender] = Proposal(_proposalUrl, payable(msg.sender), 0, 0, 0);
+        emit ProposalCreated();
     }
 
-    function voteForProposal() external {
-
+    function voteForProposal(address _proposalId) external {
+        Proposal memory thisProposal = proposals[_proposalId];
+        thisProposal.votesFor = thisProposal.votesFor + 1;
+        emit VotedFor();
     }
 
-    function voteAgainstProposal() external {
-
+    function voteAgainstProposal(address _proposalId) external {
+        Proposal memory thisProposal = proposals[_proposalId];
+        thisProposal.votesAgainst = thisProposal.votesAgainst + 1;
+        emit VotedAgainst();
     }
 
-    function fundProposal() external payable {
-
+    function fundProposal( address _proposalId) external payable {
+        require(msg.value !=0, 'Enter non empty amount');
+        Proposal memory thisProposal = proposals[_proposalId];
+        thisProposal.fundsRaised = thisProposal.fundsRaised + msg.value;
+        payable(address(this)).transfer(msg.value);
+        emit ProposalFunded();
     }
 
     function modifyProposalTime() external {
 
     }
 
-    function sendProposalFunds() external {
-
+    function sendProposalFunds(address _proposalId) external {
+        Proposal memory thisProposal = proposals[_proposalId];
+        thisProposal.proposalOwner.transfer(thisProposal.fundsRaised);
+        emit FundsDistributed();
     }
 
 
